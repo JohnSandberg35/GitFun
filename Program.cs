e2e4e6b7d6cf113700274674e1a366d36a8185a9
JohnSandberg35 <john352@byu.edu>
2026-01-28 09:36:42 -0700

    Revert "eventual mistake"
    This reverts commit 13fdff883c3fab0fde20acbd4bac537a4cd8cd30.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("This is a line of code");
-
-Console.WriteLine("This is eventually going to be a mistake");
+Console.WriteLine("This is a line of code")
